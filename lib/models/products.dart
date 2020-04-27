3a3d32d69e978e37e08bf4d6fe5334456d5154de
Narayan Singh Solnalki <hritiksingh580@gmail.com>
2020-04-27 21:26:15 +0530

    Modified Image Variable name

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -5,7 +5,7 @@ class Product {
   final String title;
   final String description;
   final double price;
-  final String imgUrl;
+  final String imageUrl;
   bool isFavorites;
 
   Product(
@@ -13,6 +13,6 @@ class Product {
       @required this.title,
       @required this.description,
       @required this.price,
-      @required this.imgUrl,
+      @required this.imageUrl,
       @required this.isFavorites = false});
 }
